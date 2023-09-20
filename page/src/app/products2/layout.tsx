f54336a9b385797c85e7af4f791607ccf80bbb0f
mason park <joung4342@gmail.com>
2023-09-20 19:13:00 +0900

    nextjs Link2

--- page/src/app/products2/layout.tsx
+++ page/src/app/products2/layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './layout.module.css';
-import Link from 'next/link'
+import Link from 'next/link';
 
 function ProductsLayout({children,}: { children: React.ReactNode }) {
     return (
--- page/src/app/products2/page.tsx
+++ page/src/app/products2/page.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import Link from 'next/link';
+
+const products = ['shirt', 'pants', 'skirt', 'shoes'];
 
 function Products2Page(props:any) {
     return (
-        <div>Products2</div>
+        <div>
+            <h1>제품을 소개 할까??</h1>
+            <ul>
+                {
+                    products.map((product,idx) => (
+                        <li key={idx}>
+                            <Link href={`/products2/${product}`}>{product}</Link>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
     );
 }
 
