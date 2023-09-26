eb371b7137078afed1c03657d8f127f75232ef5c
mason park <joung4342@gmail.com>
2023-09-26 19:53:36 +0900

    SSG 렌더링

--- rendering/src/app/products/[slug]/page.tsx
+++ rendering/src/app/products/[slug]/page.tsx
@@ -13,19 +13,19 @@ export function generateMetadata({params}: Props) {
     };
 }
 
-export default function PantsPage({params: {slug}}: Props) {
-    const product = getProduct(slug);
-  if (!product) {
-    notFound();
-  }
-    return <h1>{product} 제품 설명 페이지</h1>;
+export default async function ProductPage({params: {slug}}: Props) {
+    const product = await getProduct(slug);
+    if (!product) {
+        notFound();
+    }
+    return <h1>{product.name} 제품 설명 페이지</h1>;
 }
 
-export function generateStaticParams() {
-  // 파일에 있는 제품들을 미리 만들어보자
-  const products = getProducts();
-  return products.map((product) => ({
-    slug: product,
-  }));
+export async function generateStaticParams() {
+    // 파일에 있는 제품들을 미리 만들어보자
+    const products = await getProducts();
+    return products.map((product) => ({
+        slug: product.id,
+    }));
 }
 
